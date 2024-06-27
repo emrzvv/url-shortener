25db91e30d378937c02a6ed6291728b6294a75de
emrzvv <e.mrzvv@gmail.com>
2024-06-27 16:03:06 +0300

    wip

--- internal/app/endpoint/endpoint.go
+++ internal/app/endpoint/endpoint.go
@@ -14,29 +14,37 @@ func Shorten(w http.ResponseWriter, r *http.Request, s *storage.Storage) {
 		r.Body == nil ||
 		r.Body == http.NoBody {
 		w.WriteHeader(http.StatusBadRequest)
-	} else if body, err := io.ReadAll(r.Body); err == nil && service.IsURLValid(string(body)) {
-		w.WriteHeader(http.StatusCreated)
-		url := service.GenerateShortenURL(6)
-		for _, ok := s.Get(url); ok; url = service.GenerateShortenURL(6) {
-		}
-		s.Set(url, string(body))
-		w.Write([]byte("http://localhost:8080/" + url)) // TODO: get self address from config or smth
-	} else {
-		w.WriteHeader(http.StatusBadRequest) // TODO: dry
+		return
 	}
+
+	body, err := io.ReadAll(r.Body)
+	if err != nil || !service.IsURLValid(string(body)) {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	url := service.GenerateShortenURL(6)
+	for _, ok := s.Get(url); ok; url = service.GenerateShortenURL(6) {
+	}
+	s.Set(url, string(body))
+	w.Write([]byte("http://localhost:8080/" + url)) // TODO: get self address from config or smth
 }
 
 func GetByID(w http.ResponseWriter, r *http.Request, s *storage.Storage) {
 	if r.Method != http.MethodGet {
 		w.WriteHeader(http.StatusBadRequest)
-	} else {
-		if id := r.PathValue("id"); !service.IsIDValid(id) {
-			w.WriteHeader(http.StatusBadRequest)
-		} else if value, ok := s.Get(id); ok {
-			w.Header().Set("Location", value)
-			w.WriteHeader(http.StatusTemporaryRedirect)
-		} else {
-			w.WriteHeader(http.StatusBadRequest)
-		}
+		return
 	}
+	id := r.PathValue("id")
+	if !service.IsIDValid(id) {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	value, ok := s.Get(id)
+	if !ok {
+		w.WriteHeader(http.StatusBadRequest)
+	}
+	w.Header().Set("Location", value)
+	w.WriteHeader(http.StatusTemporaryRedirect)
 }
