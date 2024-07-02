ded14d1cb5491bc1319da7046d71a01b609a8fc4
emrzvv <e.mrzvv@gmail.com>
2024-07-02 22:37:05 +0300

    wip

--- internal/app/db/storage.go
+++ internal/app/db/storage.go
@@ -1,6 +1,9 @@
 package db
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 type Storage interface {
 	Get(key string) (string, bool)
@@ -12,28 +15,36 @@ type Storage interface {
 
 type InMemoryDB struct {
 	db map[string]string
+	m  sync.RWMutex
 }
 
 func (storage *InMemoryDB) Init() {
 	storage.db = make(map[string]string)
+	storage.m = sync.RWMutex{}
 }
 
 func NewInMemoryDBStorage(db map[string]string) Storage {
-	return &InMemoryDB{db: db}
+	return &InMemoryDB{db: db, m: sync.RWMutex{}}
 }
 
 func (storage *InMemoryDB) Get(key string) (string, bool) {
+	storage.m.RLock()
 	value, ok := storage.db[key]
+	storage.m.RUnlock()
 	return value, ok
 }
 
 func (storage *InMemoryDB) Contains(key string) bool {
+	storage.m.RLock()
 	_, ok := storage.db[key]
+	storage.m.RUnlock()
 	return ok
 }
 
 func (storage *InMemoryDB) Set(key string, value string) {
+	storage.m.Lock()
 	storage.db[key] = value
+	storage.m.Unlock()
 }
 
 func (storage *InMemoryDB) String() string {
